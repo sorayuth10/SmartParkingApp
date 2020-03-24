e89688f4707f5cede4851390fbea92a12c0c2222
o.srч <sorayuth10@gmail.com>
2020-03-24 13:57:46 +0700

    new User to login with Facebook bug fixed

--- page/container/Home.js
+++ page/container/Home.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, StatusBar, Image, ScrollView, Dimensions, } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, StatusBar, Image, ScrollView, Dimensions } from 'react-native'
 import * as firebase from 'firebase'
 import { Ionicons } from '@expo/vector-icons'
 import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu'
 import styled from 'styled-components/native'
-
 
 // CSS
 const Container = styled.View`
@@ -27,25 +26,48 @@ export default class Home extends React.Component {
   }
 
   state = {
-    arrangePlace: []
+    arrangePlace: [],
+    showFirstAddData: true
   }
 
   componentDidMount() {
-    if (!firebase.auth().currentUser.displayName) {
-      this.props.navigation.navigate('NewProfile')
-    } else {
-      // this.props.navigation.navigate('Home')
-      let arrangePlace = []
-      firebase
-        .database()
-        .ref('Devices')
-        .on('value', (data) => {
-          arrangePlace = Object.values(data.val()).map(({ Place: { img, name } }) => ({ img, name }))
-          const filteredArrangePlace = arrangePlace.filter(
-            ({ name }, index, selfs) => selfs.findIndex((self) => self.name === name) === index
-          )
-          this.setState(() => ({ arrangePlace: filteredArrangePlace }))
-        })
+    firebase
+      .database()
+      .ref('UserAuth/' + firebase.auth().currentUser.uid)
+      .once('value', (snap) => {
+        if (!snap.exists()) {
+          this.props.navigation.navigate('NewProfile')
+        } else {
+          // this.props.navigation.navigate('Home')
+          this.toggleFirstAddData()
+          let arrangePlace = []
+          firebase
+            .database()
+            .ref('Devices')
+            .on('value', (data) => {
+              arrangePlace = Object.values(data.val()).map(({ Place: { img, name } }) => ({ img, name }))
+              const filteredArrangePlace = arrangePlace.filter(
+                ({ name }, index, selfs) => selfs.findIndex((self) => self.name === name) === index
+              )
+              this.setState(() => ({ arrangePlace: filteredArrangePlace }))
+            })
+        }
+      })
+  }
+
+  toggleFirstAddData = () => {
+    this.setState({
+      showFirstAddData: false
+    })
+  }
+
+  _renderFirstAddData() {
+    if (this.state.showFirstAddData) {
+      return (
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('NewProfile')}>
+          <Text>Add car data</Text>
+        </TouchableOpacity>
+      )
     }
   }
 
@@ -53,11 +75,7 @@ export default class Home extends React.Component {
     this._menu.hide()
     this.props.navigation.navigate('Profile')
   }
-  // handleParking = (name) => {
-  //   this.props.navigation.navigate('Parking', { namePlace: { name } })
-  //   console.log(name)
-  //   //send params...
-  // }
+
   handleAbouUs = () => {
     this._menu.hide()
     this.props.navigation.navigate('AboutUs')
@@ -65,10 +83,11 @@ export default class Home extends React.Component {
   signOutUser = () => {
     firebase.auth().signOut()
   }
+
   placeList() {
     return this.state.arrangePlace.map(({ img, name }, index) => (
       <React.Fragment key={index}>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Parking', { namePlace: {name} })}> 
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('Parking', { namePlace: { name } })}>
           <View style={styles.placeItem}>
             <Image style={{ width: Dimensions.get('window').width / 2.02, height: 125 }} source={{ uri: img }} />
             <Text key={index} style={{ alignSelf: 'center' }}>
@@ -85,40 +104,32 @@ export default class Home extends React.Component {
       <Container>
         {/* dark-content Status bar */}
         <StatusBar barStyle="dark-content" backgroundColor="#EBECF4" animated={true} />
-          <View style={styles.header}>
-            <Image
-              style={{ 
-                width: Dimensions.get('window').width / 5,
-                height: Dimensions.get('window').height / 10.1, 
-                alignItems: 'flex-start', 
-                marginLeft: 20,
-                marginTop:40
 
-              }}
-              source={require('../../image/logo.png')}
-            />
-            
-            <Image
-              style={{
-                width: Dimensions.get('window').width / 5,
-                height: Dimensions.get('window').height / 10,
-                marginRight: 25,
-                marginTop:40
-               
-              }}
-              source={require('../../image/account.png')}
-            />
-           
-          
-            
-            
-            
-         
-            
-         
-            
+        <View style={styles.header}>
+          <Image
+            style={{
+              width: '28%',
+              height: '75%',
+              alignItems: 'flex-start',
+              marginLeft: 20,
+              marginTop: 40
+            }}
+            source={require('../../image/logo.png')}
+          />
+          <Text style={styles.headerTitle}>PLACE</Text>
+          <Image
+            style={{
+              width: '20%',
+              height: '50%',
+              marginRight: 25,
+              marginTop: 70,
+              borderRadius: 50
+            }}
+            source={require('../../image/account.png')}
+          />
+
           <View style={styles.menumore}>
-            {/* <Menu
+            <Menu
               ref={this.setMenuRef}
               button={
                 <Text style={{ paddingLeft: 19, marginTop: 52 }} onPress={this.showMenu}>
@@ -130,17 +141,14 @@ export default class Home extends React.Component {
               <MenuItem onPress={this.handleAbouUs}>About Us</MenuItem>
               <MenuDivider />
               <MenuItem onPress={this.signOutUser}>Log out</MenuItem>
-            </Menu> */}
-           
+            </Menu>
           </View>
         </View>
-          <View style={styles.text}>
-            <Text style={{ alignSelf: 'center', fontWeight: 'bold', fontSize: 20, marginBottom: 20}}>PLACE</Text>
-          </View>
-        
-        <ScrollView>
+
+        <ScrollView style={{ marginTop: -50 }}>
           <List>{this.placeList()}</List>
         </ScrollView>
+        {this._renderFirstAddData()}
       </Container>
     )
   }
@@ -151,30 +159,25 @@ const styles = StyleSheet.create({
     paddingTop: 35,
     paddingBottom: 15,
     backgroundColor: '#ED9703',
-    // borderBottomWidth: 15,
-    // borderBottomColor: '#A9A9A9',
-    // shadowOffset: { height: 5,width: 100 },
-    // shadowRadius: 15,
-    // shadowOpacity: 0.2,
     justifyContent: 'space-between',
     flexDirection: 'row',
-    borderRadius: 80,
-    height: 175,
-    bottom:30
-    
-     
+    borderRadius: 60,
+    height: 190,
+    bottom: 75,
+    marginTop: 20
   },
   headerTitle: {
-    fontSize: 20,
-    fontWeight: '500'
+    top: 50,
+    alignSelf: 'center',
+    fontWeight: '900',
+    fontSize: 20
   },
-  
   menumore: {
     position: 'absolute',
     top: 100,
     right: 10,
     width: 40,
-    height: 35,
+    height: 35
     // backgroundColor: 'rgba(21, 22, 48, 0.1)',
   }
 })
