cc3dc10bc0702bc1bd52bde146f8a738690568fe
o.srч <sorayuth10@gmail.com>
2020-04-20 13:26:48 +0700

    change to ReactHook

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity, StatusBar, Image, ScrollView, Dimensions } from 'react-native'
 import * as firebase from 'firebase'
 import styled from 'styled-components/native'
@@ -13,29 +13,26 @@ const List = styled.View`
   flex-wrap: wrap;
   justify-content: space-between;
 `
-const resetAction = StackActions.reset({
-  index: 0,
-  actions: [NavigationActions.navigate({ routeName: 'NewProfile' })]
-})
 
-const urlDefault = '../../assets/account.png'
-export default class Home extends React.Component {
-  state = {
-    arrangePlace: [],
-    urlDefault: ''
-  }
+export const Home = (props) => {
+  const resetAction = StackActions.reset({
+    index: 0,
+    actions: [NavigationActions.navigate({ routeName: 'NewProfile' })]
+  })
+  const { navigation } = props
+  const [arrangePlace, setArrangePlace] = useState([])
+  const [urlDefault, setUrlDefault] = useState(require('../../assets/account.png'))
 
-  componentDidMount() {
-    console.log(firebase.auth().currentUser.photoURL)
-    if (firebase.auth().currentUser.photoURL) {
-      this.setState({ urlDefault: firebase.auth().currentUser.photoURL })
+  useEffect(() => {
+    if (firebase.auth().currentUser.photoURL != null) {
+      setUrlDefault({ uri: firebase.auth().currentUser.photoURL })
     }
     firebase
       .database()
       .ref('UserAuth/' + firebase.auth().currentUser.uid)
       .once('value', (snap) => {
         if (!snap.exists()) {
-          this.props.navigation.dispatch(resetAction)
+          navigation.dispatch(resetAction)
         } else {
           let arrangePlace = []
           firebase
@@ -46,27 +43,20 @@ export default class Home extends React.Component {
               const filteredArrangePlace = arrangePlace.filter(
                 ({ name }, index, selfs) => selfs.findIndex((self) => self.name === name) === index
               )
-              this.setState(() => ({ arrangePlace: filteredArrangePlace }))
+              setArrangePlace(filteredArrangePlace)
             })
         }
       })
+  }, [])
+
+  const handleProfile = () => {
+    navigation.navigate('Profile')
   }
 
-  handleProfile = () => {
-    this.props.navigation.navigate('Profile')
-  }
-
-  handleAbouUs = () => {
-    this.props.navigation.navigate('AboutUs')
-  }
-  signOutUser = () => {
-    firebase.auth().signOut()
-  }
-
-  _placeList() {
-    return this.state.arrangePlace.map(({ img, name }, index) => (
+  const _placeList = () => {
+    return arrangePlace.map(({ img, name }, index) => (
       <React.Fragment key={index}>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Parking', { namePlace: { name } })}>
+        <TouchableOpacity onPress={() => navigation.navigate('Parking', { namePlace: { name } })}>
           <View style={styles.placeItem}>
             <Image style={{ width: Dimensions.get('window').width / 2.02, height: 125 }} source={{ uri: img }} />
             <Text key={index} style={{ alignSelf: 'center' }}>
@@ -78,40 +68,39 @@ export default class Home extends React.Component {
     ))
   }
 
-  render() {
-    return (
-      <Container>
-        {/* dark-content Status bar */}
-        <StatusBar barStyle="dark-content" backgroundColor="#EBECF4" animated={true} />
+  return (
+    <Container>
+      {/* dark-content Status bar */}
+      <StatusBar barStyle="dark-content" backgroundColor="#EBECF4" animated={true} />
 
-        <View style={styles.header}>
+      <View style={styles.header}>
+        <Image
+          style={{
+            width: '28%',
+            height: '75%',
+            alignItems: 'flex-start',
+            marginLeft: 10,
+            marginTop: 40
+          }}
+          source={require('../../assets/logo.png')}
+        />
+        <Text style={styles.headerTitle}>PLACES</Text>
+        <TouchableOpacity onPress={handleProfile}>
           <Image
-            style={{
-              width: '28%',
-              height: '75%',
-              alignItems: 'flex-start',
-              marginLeft: 10,
-              marginTop: 40
-            }}
-            source={require('../../assets/logo.png')}
+            style={{ marginTop: 70, marginRight: 15, width: 70, height: 70, borderRadius: 50 }}
+            source={urlDefault}
           />
-          <Text style={styles.headerTitle}>PLACES</Text>
-          <TouchableOpacity onPress={this.handleProfile}>
-            <Image
-              style={{ marginTop: 70, marginRight: 15, width: 70, height: 70, borderRadius: 50 }}
-              source={require(urlDefault)}
-            />
-            {/* {uri: firebase.auth().currentUser.photoURL} */}
-          </TouchableOpacity>
-        </View>
+          {/* {uri: firebase.auth().currentUser.photoURL} */}
+        </TouchableOpacity>
+      </View>
 
-        <ScrollView style={{ marginTop: -50 }}>
-          <List>{this._placeList()}</List>
-        </ScrollView>
-      </Container>
-    )
-  }
+      <ScrollView style={{ marginTop: -50 }}>
+        <List>{_placeList()}</List>
+      </ScrollView>
+    </Container>
+  )
 }
+export default Home
 
 const styles = StyleSheet.create({
   header: {
